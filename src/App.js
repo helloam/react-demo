02f92100b4e4f77d0946fc0679c7646d4223f518
Angeli <angeli.mateo@gmail.com>
2023-05-05 16:01:23 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-        <p>"The future belongs to those who believe in the beauty of their dreams."
-        </p>
-        <a
-          className="App-link"
-          href="https://www.womenshistory.org/education-resources/biographies/eleanor-roosevelt"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         -Eleanor Roosevelt
-        </a>
-      </header>
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
